f0d75cf18d643e75ca2834825d5eb4e6b68526f1
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-21 17:03:51 +0000

    fix install

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub fn setup(version: String) -> FnResult<String> {
 
     let stdout = dag()
         .mise()?
-        .with_exec(vec!["mise", "install", &format!("java@{}", version)])?
+        .with_exec(vec!["mise", "install", &format!("java{}", version)])?
         .with_exec(vec!["mise", "use", "-g", "java"])?
         .stdout()?;
     Ok(stdout)
